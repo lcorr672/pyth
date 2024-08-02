a5b3f35bc3454faa6d190b979839e8131aaf8eee
lcorr672 <lcorr672@evit.edu>
2024-08-02 11:55:10 -0700

    Add files via upload

---
+++ Mad lib.py
@@ -0,0 +1,11 @@
+noun1 = input('Please put a noun: ') 
+verb1 = input('Please insert a verb 1: ') 
+noun2 = input('Noun2: ') 
+place = input('place: ')
+noun3 = input('noun3: ') 
+#put the verbs in one spot 
+
+#made the story as I went for organizing and ezz
+madlib = f'Walking down the street I see {noun1}. and they were {verb1}. After the preformance set by the {verb1}. A dog toled me that he needed help carrying a {noun2}. Which I carried it to {place} placing it on top of a {noun3}.' 
+
+print(madlib) 
